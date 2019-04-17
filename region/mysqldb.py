3593df260b7893e71203af8bd392c367359baded
zpp360 <184458308@qq.com>
2019-04-17 14:42:31 +0800

    inti

---
+++ region/mysqldb.py
@@ -0,0 +1,39 @@
+# coding = utf-8
+import pymysql
+class mysqldb:
+    def __init__(self,host,user,passwd,db,port,charset):
+        self.host = host
+        self.user = user
+        self.passwd = passwd
+        self.db = db
+        self.port = port
+        self.charset = charset
+
+    def get_connection(self):
+        conn = pymysql.connect(host=self.host, user=self.user, passwd=self.password, db=self.db, port=self.port, charset=self.charset)
+        return conn
+
+    def get_cursor(self,conn):
+        cur = conn.cursor()
+        return cur
+
+    def insert_region_dict(self,region_dict):
+        try:
+            conn = self.get_connection()
+            cur = self.get_cursor(conn)
+            insert_sql = "insert into m_region(region_code,region_name,p_code,p_name) values"
+            param = ""
+            for county_code in region_dict.keys():
+                county_region = region_dict.get(county_code)
+                param = param + "('{}','{}','{}','{}'),".format(county_region.code, county_region.name,
+                                                                county_region.parent_code, county_region.parent_name)
+            param = param[0:len(param) - 1] + ""
+            insert_sql = insert_sql + param
+            cur.execute(insert_sql)
+            conn.commit()
+        except Exception:
+            print("插入数据发生异常")
+            conn.rollback()
+        finally:
+            cur.close()
+            conn.close()
--- region/regionSpider.py
+++ region/regionSpider.py
@@ -1,27 +1,41 @@
 # coding = utf-8
 # 爬取国家统计局地域代码
-
+import urllib3
 import urllib.request
 from bs4 import BeautifulSoup
 import re
 from region import region
+import pymysql
+import random
 class regionSpider:
     def __init__(self):
-        self.url = 'http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2018/37.html'
-        self.cityUrls = set()
         self.city_tr_class = 'citytr'
         self.county_tr_class = 'countytr'
         self.town_tr_class = 'towntr'
         self.village_tr_class = 'villagetr'
 
     def getHtml(self,url):
-        page = urllib.request.urlopen(url)
+        USER_AGENTS = [
+            "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
+            "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
+            "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
+            "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)",
+            "Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
+            "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
+            "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0",
+            "Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5"
+        ]
+        user_agent = random.choice(USER_AGENTS)
+        headers = ('User-Agent', user_agent)
+        opener = urllib.request.build_opener()
+        opener.addheaders = [headers]
+        page = opener.open(url+"?COLLCC=46711612&")
         html = page.read().decode(encoding='gb2312', errors='ignore')
         return html
 
     def getCityNameAndUrl(self,url):
         dict = {}
-        html = self.getHtml(self.url)
+        html = self.getHtml(url)
         soup = BeautifulSoup(html,'html.parser')
         city_trs = soup.find_all(class_=self.city_tr_class)
         for tr in city_trs:
@@ -131,12 +145,33 @@ for city_code in city_dict.keys():
             town_dicts = dict(town_dicts,**town_dict)
             for town_code in town_dict:
                 #循环遍历街道（镇），获取社区（村）
-                print(town_dicts.get(town_code))
-                town_region = town_dicts.get(town_code)
+                print(town_dict.get(town_code))
+                town_region = town_dict.get(town_code)
                 village_dict = spider.getVillageNameAndUrl(town_region)
                 if None!=village_dict:
                     village_dicts = dict(village_dicts,**village_dict)
                     for village_code in village_dict.keys():
-                        print(village_dicts.get(village_code))
+                        print(village_dict.get(village_code))
+#使用pymysql将数据插入数据库
+try:
+    conn = pymysql.connect(host='192.168.0.116', user='root', passwd='sh123456', db='shuheng', port=3306,charset='utf8')
+    cur = conn.cursor()
+    insert_sql = "insert into m_region(region_code,region_name,p_code,p_name) values"
+    param = ""
+    for county_code in county_dicts.keys():
+        county_region = county_dicts.get(county_code)
+        param = param + "('{}','{}','{}','{}'),".format(county_region.code, county_region.name,county_region.parent_code, county_region.parent_name)
+    param = param[0,len(param)-1]
+    insert_sql = insert_sql + param
+    print(insert_sql)
+    cur.execute(insert_sql)
+    conn.commit()
+except Exception:
+    conn.rollback()
+    print("发生异常")
+
+
+
+
 
 
